d714eb67d3341a6f71a4f194eab84e99942928bb
Cloud1379 <122495886+Cloud1379@users.noreply.github.com>
2024-05-06 12:43:06 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,33 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const hornSelect = document.getElementById("horn-select");
+    const image = document.querySelector("#expose img");
+    const audio = document.querySelector("#expose audio");
+
+    hornSelect.addEventListener("change", (event) => {
+        const selectedHorn = event.target.value;
+    
+        switch (selectedHorn) {
+          case "air-horn":
+            image.src = "assets/images/air-horn.svg";
+            image.alt = "Air Horn";
+            audio.src = "assets/audio/air-horn.mp3";
+            break;
+          case "car-horn":
+            image.src = "assets/images/car-horn.svg";
+            image.alt = "Car Horn";
+            audio.src = "assets/audio/car-horn.mp3";
+            break;
+          case "party-horn":
+            image.src = "assets/images/party-horn.svg";
+            image.alt = "Party Horn";
+            audio.src = "assets/audio/party-horn.mp3";
+            break;
+          default:
+            image.src = "assets/images/no-image.png";
+            image.alt = "No image selected";
+            audio.src = "";
+        }
+      });
 }
